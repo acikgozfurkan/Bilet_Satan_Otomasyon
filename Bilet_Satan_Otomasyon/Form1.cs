05f666283c2d48ca657d822ba9bab2c69f286002
Acer <Acer@DESKTOP-1I9R89O>
2021-02-03 02:30:45 +0300

    03.02.2021

--- Bilet_Satan_Otomasyon/Form1.cs
+++ Bilet_Satan_Otomasyon/Form1.cs
@@ -12,9 +12,31 @@ namespace Bilet_Satan_Otomasyon
 {
     public partial class Form1 : Form
     {
+
+        DataTable tablo = new DataTable();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tablo.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text, comboBox1.Text);
+            dataGridView1.DataSource = tablo;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablo.Columns.Add("Ad", typeof(string));
+            tablo.Columns.Add("Soyad", typeof(string));
+            tablo.Columns.Add("yas", typeof(int));
+            tablo.Columns.Add("Cinsiyet", typeof(string));
+            dataGridView1.DataSource = tablo;
         }
     }
 }
